035b8d421aa9293c126c423b19d72dfe996ce601
kyChu <hello.kychu@gmail.com>
2017-12-15 09:40:57 +0800

    first commit

---
+++ src/MainFrame/MainFrame.java
@@ -0,0 +1,54 @@
+package MainFrame;
+
+import javax.swing.JFrame;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class MainFrame extends JFrame{
+	private static final long serialVersionUID = 1L;
+	public MainFrame() {
+		/* Create JTree */
+		DefaultMutableTreeNode TopNode = new DefaultMutableTreeNode("Project Explorer");
+		DefaultMutableTreeNode category = null;
+		DefaultMutableTreeNode book = null;
+		category = new DefaultMutableTreeNode("Books for Java Programmers");
+		TopNode.add(category);
+		//original Tutorial  
+	    book = new DefaultMutableTreeNode("The Java Tutorial: A Short Course on the Basics");
+	    category.add(book);
+
+	    //Tutorial Continued  
+	    book = new DefaultMutableTreeNode("The Java Tutorial Continued: The Rest of the JDK");
+	    category.add(book);
+
+	    //JFC Swing Tutorial  
+	    book = new DefaultMutableTreeNode("The JFC Swing Tutorial: A Guide to Constructing GUIs");
+	    category.add(book);  
+
+	    category = new DefaultMutableTreeNode("Books for Java Implementers");  
+	    TopNode.add(category);
+
+	    //VM
+	    book = new DefaultMutableTreeNode("The Java Virtual Machine Specification");  
+	    category.add(book);
+
+	    //Language Spec
+	    book = new DefaultMutableTreeNode("The Java Language Specification");  
+	    category.add(book);
+
+		JTree FileBrowser = new JTree(TopNode);
+		this.add(FileBrowser);
+
+		/* set frame window */
+		this.setTitle("Project Builder V0.0.1");
+		this.setSize(600, 400);
+		this.setResizable(true);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+	public static void main(String[] args) {
+		new MainFrame();
+		System.out.println("Hello World!");
+	}
+}
